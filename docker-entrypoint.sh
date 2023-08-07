2d1cfb7749f2648515c34681dda06720a4cc6db2
Juan Carlos A <abjuancarlos.12@gmail.com>
2023-08-06 21:05:18 -0500

    update super user creation

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,11 +2,12 @@
 
 # apply database migrations
 echo "---apply database migrations---"
+/root/env/bin/python manage.py makemigrations --settings=core.settings.local
 /root/env/bin/python manage.py migrate --settings=core.settings.local
 
 # create superuser by default
 echo "---create superuser---"
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell --settings=core.settings.local
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'Admin12345678#')" | python manage.py shell --settings=core.settings.local
 
 # runserver
 echo "---run server---"
